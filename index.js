1107b57044a45bf69b5fe516d90305edece5b09b
Nicolas Bermudez <bermnicolas@gmail.com>
2019-05-04 02:05:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+  let min = array[0];
+  let minIndex = 0;
+  array.filter((int, index) => {
+    if (int < min) {
+      min = int;
+      minIndex = index;
+    }
+  })
+  array.splice(minIndex, 1)
+  return min;
 }
 
-function insertionSort(array){
-
+function insertionSort(array) {
+  let newArray =[];
+  while(array.length >= 1) {
+    newArray.push(findMinAndRemove(array))
+  }
+  return newArray;
 }
