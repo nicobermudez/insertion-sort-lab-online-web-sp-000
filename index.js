b429e6c7f6627d950c73bd86c0eaa755ff0296b3
Nicolas Bermudez <bermnicolas@gmail.com>
2019-05-04 02:01:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function findMinAndRemove(array){
   let min = array[0];
+  let minIndex = 0;
   array.filter(int => {
     if (int < min) {
       min = int;
     }
   })
+  array.splice(minIndex, 1)
   return min;
 }
 
